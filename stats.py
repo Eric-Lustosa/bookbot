b74c9570ec4d575ed338f2480376042b8cfaf56e
Eric-Lustosa <eric.g.lustosa@gmail.com>
2025-03-19 02:51:22 +0100

    Adicionado comentários em cada uma das funções da stats.py

--- stats.py
+++ stats.py
@@ -1,30 +1,28 @@
 
-
+#Lê arquivo.txt dentro do diretório books e retorna uma string que contem todo o texto do livro
 def get_book_text(filepath):   
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
+#Usando o comando split armazena em uma variável uma lista com todas as palavras do livro em seguida conta-se a quantidade de palavras usando a função len
 def get_book_words(file_contents):
     words = file_contents.split()
     num_words = len(words)
     return num_words
-
+#É preenchido um dicionário vazio, sendo as chaves cada character existente no livro e o seu valor associado sendo o número de vezes que ele aparece no livro
 def get_book_char(file_contents):
     dic_char = {}
     for i in file_contents:
-        i_lower = i.lower()
+        i_lower = i.lower() #Transforma em lower case
         if i_lower in dic_char:
-            dic_char[i_lower] = dic_char.get(i_lower, 1) + 1
+            dic_char[i_lower] = dic_char.get(i_lower, 1) + 1 # Preenche o dicionário com a chave e o valor incrementado, caso não exista ainda essa chave ela recebe o valor default 1
         else:
             dic_char[i_lower] = 1
     return dic_char
 
-
-#def sort_on(dic):
-    #return dic[a]
            
-def get_dict_list(dic):
+def get_dict_list(dic): # função que ordena em ordem decrescente do número de vezes que o character aparece no livro em uma lista
     dic_list = []
 
     for chave in dic:
@@ -37,7 +35,7 @@ def get_dict_list(dic):
 
     return sorted_items
 
-def print_report(sorted_items,file_contents):
+def print_report(sorted_items,file_contents): # função que imprime o relatório final com todos os caracteres em ordem decrescente de quantidade e o número total de palavras
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt")
     print("----------- Word Count ----------")
